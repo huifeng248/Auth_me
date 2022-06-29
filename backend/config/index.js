6f48d941ce0d79d7c76886f0e5f3e942fe1bdeaa
Hui Feng <huifeng248@gmail.com>
2022-06-29 16:29:17 -0700

    phase 1-4

---
+++ backend/config/index.js
@@ -0,0 +1,15 @@
+// backend/config/index.js
+module.exports = {
+    environment: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8000,
+    dbFile: process.env.DB_FILE,
+    jwtConfig: {
+      secret: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN
+    }
+  };
+
+  //you will create a js configuration file that will 
+  //read the environment variables loaded and export them.
+  //Each environment variable will be read and exported 
+  //as a key from this file.
